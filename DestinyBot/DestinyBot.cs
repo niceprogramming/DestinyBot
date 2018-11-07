8d19030f370933892a193cf75f64564083b655c9
niceprogramming <adamkallenbach@gmail.com>
2018-11-06 21:36:16 -0600

    added packages and config model

--- DestinyBot/DestinyBot.cs
+++ DestinyBot/DestinyBot.cs
@@ -3,11 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
+using DestinyBot.Models;
+using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
 
 namespace DestinyBot
 {
     public class DestinyBot
     {
-        
+        private readonly DiscordSocketClient _client;
+        private readonly IConfiguration _config;
+        private readonly IServiceProvider _services;
+        public DestinyBot()
+        {
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                MessageCacheSize = 1000,
+                AlwaysDownloadUsers = true,
+#if DEBUG
+                LogLevel = LogSeverity.Verbose,
+#else
+                LogLevel = LogSeverity.Info,
+#endif
+                DefaultRetryMode = RetryMode.AlwaysRetry
+            });
+        }
+
+        public async Task StartAsync()
+        {
+            
+
+            await _client.LoginAsync(TokenType.Bot, _config.Get<BotConfig>().DiscordToken);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
     }
 }
---
+++ DestinyBot/Models/BotConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DestinyBot.Models
+{
+    public class BotConfig
+    {
+        public string ConnectionString { get; set; }
+        public string YoutubeKey { get; set; }
+        public string TwitchClientId { get; set; }
+        public string DiscordToken { get; set; }
+        public string ImgurClientId { get; set; }
+        public string ImgurAlbumId { get; set; }
+        public string Prefix { get; set; }
+        public int ThrottleLength { get; set; }
+    }
+}
