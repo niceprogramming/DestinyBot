5b40912cafe39bfb5d5d95566b434a3100595730
niceprogramming <adamkallenbach@gmail.com>
2019-04-13 10:56:36 -0500

    Twitch End is now set to zero on start

--- DestinyBot/Jobs/TwitchJob.cs
+++ DestinyBot/Jobs/TwitchJob.cs
@@ -44,14 +44,12 @@ namespace DestinyBot.Jobs
                 if (streamer is null) return;
 
                 var isStreaming = _twitchService.IsStreamLiveAsync(streamer.Name).GetAwaiter().GetResult();
-                
+
                 //Twitch api is a dumpsterfire and says the stream is on even after it stops for example it will go null -> null -> On -> null -> null.
                 //we wait for 3 minutes after the last stream to make sure the streamer in actually on or offline
                 //this is arbitrary and seems like it just works
-                if (DateTimeOffset.UtcNow - DateTimeOffset.FromUnixTimeSeconds(streamer.StreamEndTime) < TimeSpan.FromMinutes(3))
-                {
-                    return;
-                }
+                if (DateTimeOffset.UtcNow - DateTimeOffset.FromUnixTimeSeconds(streamer.StreamEndTime) <
+                    TimeSpan.FromMinutes(3)) return;
 
                 //stream has ended and we are waiting for startup again
                 if (!isStreaming && streamer.StreamLength >= TimeSpan.Zero) return;
@@ -61,6 +59,7 @@ namespace DestinyBot.Jobs
                     var stream = _twitchService.GetStreamAsync(streamer.Name).GetAwaiter().GetResult();
 
                     streamer.SteamStartTime = stream.StartedAt.ToUnixTimeSeconds();
+                    streamer.StreamEndTime = 0;
                     var game = _twitchService.GetGame(stream.GameId).GetAwaiter().GetResult();
                     streamer.Games.Add(new Game
                     {
@@ -121,7 +120,8 @@ namespace DestinyBot.Jobs
                             return;
                         }
 
-                        message.ModifyAsync(x => x.Embed = CreateTwitchEmbed(streamer,subscription, stream, logo)).GetAwaiter()
+                        message.ModifyAsync(x => x.Embed = CreateTwitchEmbed(streamer, subscription, stream, logo))
+                            .GetAwaiter()
                             .GetResult();
                     }
                 }
@@ -131,7 +131,7 @@ namespace DestinyBot.Jobs
                 {
                     streamer.Games.Last().EndTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
                     streamer.StreamEndTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-                    
+
                     var user = _twitchService.GetUserAsync(streamer.Name).GetAwaiter().GetResult();
 
                     var description = new StringBuilder();
@@ -164,7 +164,7 @@ namespace DestinyBot.Jobs
                             channel.GetMessageAsync((ulong) subscription.MessageId).GetAwaiter()
                                 .GetResult() as IUserMessage;
 
-                        
+
                         if (message is null)
                         {
                             Log.Information("Message was not found");
@@ -188,16 +188,19 @@ namespace DestinyBot.Jobs
             var channel = _client.GetChannel(Convert.ToUInt64(subscription.DiscordChannelId)) as ITextChannel;
 
             var message =
-                await channel.SendMessageAsync(string.Empty, embed: CreateTwitchEmbed(streamer, subscription, stream, logoUrl));
+                await channel.SendMessageAsync(string.Empty,
+                    embed: CreateTwitchEmbed(streamer, subscription, stream, logoUrl));
             return (long) message.Id;
         }
 
-        private Embed CreateTwitchEmbed(TwitchStreamer streamer,TwitchSubscription subscription, Stream stream, string logoUrl)
+        private Embed CreateTwitchEmbed(TwitchStreamer streamer, TwitchSubscription subscription, Stream stream,
+            string logoUrl)
         {
             var timeLive = DateTimeOffset.UtcNow - DateTimeOffset.FromUnixTimeSeconds(streamer.SteamStartTime);
             var game = _twitchService.GetGame(stream.GameId).GetAwaiter().GetResult();
             return new EmbedBuilder()
-                .WithAuthor($"{streamer.Name} is live", url: subscription.AlternateLink ?? $"https://twitch.tv/{streamer.Name}")
+                .WithAuthor($"{streamer.Name} is live",
+                    url: subscription.AlternateLink ?? $"https://twitch.tv/{streamer.Name}")
                 .WithTitle($"{stream.Title}")
                 .WithUrl(subscription.AlternateLink ?? $"https://twitch.tv/{streamer.Name}")
                 .WithThumbnailUrl(logoUrl)
