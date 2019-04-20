719b7582da8f37812c37b3119c20b80cf776e616
niceprogramming <adamkallenbach@gmail.com>
2019-04-19 20:33:18 -0500

    Add stream message

---
+++ DestinyBot.Data/Migrations/20190420011743_streammessage.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DestinyBot.Data.Migrations
+{
+    public partial class streammessage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                "Message",
+                "TwitchSubscriptions",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                "Message",
+                "TwitchSubscriptions");
+        }
+    }
+}
